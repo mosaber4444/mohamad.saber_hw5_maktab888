bfb83aabf4b2163800a4d06e832a23770ad460e8
Mohamad Saber <mohamadsaber557@gmail.com>
2022-12-07 11:30:48 +0330

    add js 3

--- js/3/1.js
+++ js/3/1.js
@@ -1,14 +1,31 @@
-// q2
+// q3
 
-let number = 5215861 ;
+let input = -2252552455861;
 
-// function separators(num){
-//     let text = num ;
-//     let len = number.length ;
-//     for (let i = 0 ; i < len ; i ++){
-//
-//     }
-// }
-console.log(
-https://github.com/mosaber4444/mohamad.saber_hw5_maktab888.git
-)
+function separators(input) {
+    if (typeof input !== 'number'){
+        return 'input is not number'
+    }
+    let neg = false;
+    if (input < 0) {
+        neg = true;
+        input = -input
+    }
+    let num = String(input);
+    let len = num.length;
+    let doThis = len % 3;
+    let text = '';
+    if (doThis !== 0) {
+        text = num.slice(0, doThis) + ','
+    }
+    for (let i = doThis; i < len; i = 3 + i) {
+        text = text + num.slice(i, i + 3) + ',';
+    }
+    text = text.slice(0, -1)
+    if (neg) {
+        text = '-' + text;
+    }
+    return text ;
+}
+
+console.log(separators(input))
